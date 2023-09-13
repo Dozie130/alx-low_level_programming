60365cdc111e8fcaeffeb1cfd69b7948754adce6
Dozie130 <dozie.ebuka@gmail.com>
2023-09-13 07:06:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void _putchar(char character) {
+  printf("%c", character);
+}
+
+int main() {
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('\n');
+
+  return 0;
+}
